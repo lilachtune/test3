8eaf5ed7b29ce3be6e9692d92c393c00f0eb9904
lilachtune <yahowasu@gmail.com>
2026-01-17 17:45:56 +1000

    added some stuff

--- test1.py
+++ test1.py
@@ -1,2 +1,13 @@
+import random
+import time
+
+def generate_data():
+    # Простой генератор
+    return {
+        "heart_rate": random.randint(60, 120),
+        "steps": random.randint(0, 100)
+    }
+
+
 def piska():
     print()
